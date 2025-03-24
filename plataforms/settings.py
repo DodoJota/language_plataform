d919acebcdcbe4238cde8ff98aa188f6578e3b37
DodoJota <dorianjr8@gmail.com>
2025-03-24 19:38:23 +0000

    statifiles fixed

--- plataforms/settings.py
+++ plataforms/settings.py
@@ -144,10 +144,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'language/media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'language/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'language/static'),
 ]
 
 # Default primary key field type
