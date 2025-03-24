90dfd4d976eb8239a55354b30ff6c1ed458711c7
DodoJota <dorianjr8@gmail.com>
2025-03-24 19:28:06 +0000

    statifiles fixed

--- plataforms/settings.py
+++ plataforms/settings.py
@@ -144,7 +144,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'language/media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'language/staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
